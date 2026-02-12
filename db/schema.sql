b4c0b0a2888c2b8065484a04960ad1180d50bb9f
minwooHwang <has710312@gmail.com>
2026-02-12 17:01:11 +0900

    feat: 코드 초안 작성

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,25 @@
 /*
 테이블 구조(스키마) 정의
 */
+-- 채용공고 테이블(ETL_jumpit_jobs.csv)
+
+DROP TABLE IF EXISTS jumpit_jobs;
+CREATE TABLE jumpit_jobs(
+    id SERIAL PRIMARY KEY,
+    url TEXT,
+    title TEXT,
+    company_name TEXT,
+    tech_stack TEXT,
+    work TEXT,
+    qualification TEXT,
+    prefer TEXT,
+    benefit TEXT,
+    process TEXT,
+    work_experience TEXT,
+    education TEXT,
+    deadline TEXT,
+    location TEXT,
+    tech_stack_list TEXT,
+    location_state TEXT,
+    deadline_dt DATE,
+)
