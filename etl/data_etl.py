a1a7cb61d76400317cf9d6ff09e35807569fea6a
minwooHwang <has710312@gmail.com>
2026-02-17 15:14:07 +0900

    feat: ETL 코드 수정 - 학력컬럼에 대해서 학력별로 INT Level 컬럼 만듦 - 경력사항 컬럼에 대해 최대경력, 최소 경력 컬럼(INT)을 만듦

--- etl/data_etl.py
+++ etl/data_etl.py
@@ -3,6 +3,7 @@
 #######
 import pandas as pd
 import json
+import re
 
 # 파일 경로 설정
 INPUT_PATH = "data/jumpit_jobs.csv"
@@ -170,12 +171,96 @@ def save_csv_with_json_lists(df, list_cols=("tech_stack",)):
     return df_out
     
 
+# education 컬럼에 대해서 학력별로 INT LEVEL 컬럼을 만들어주는 함수
+'''
+pd.NA -> 결측값 자체를 나타내는 값(상수)
+pd.isna(x) -> x가 결측값이면 True, 아니면 False를 반환하는 함수
+pd.notna(x) -> x가 결측값이 아니면 True, 아니면 False를 반환하는 함수
+'''
+def education_INT_LEVEL(df, source_col="education", target_col="education_level"):
+    def map_level(text):
+        if pd.isna(text):
+            return pd.NA
+        
+        if "박사" in text:
+            return 5
+        if "석사" in text:
+            return 4
+        if "학사" in text or "대학교" in text or "대졸" in text:
+            return 3
+        if "2,3년" in text or "2~3년" in text or "전문대" in text:
+            return 2
+        if "고졸" in text or "학력무관" in text or "무관" in text:
+            return 1
+        
+        return pd.NA
+    df[target_col] = df[source_col].apply(map_level)
+    
+    return df
+
+# work_experience 컬럼에 대해서 최대·최소 값을 가지는 컬럼 2개로 쪼개는 함수
+def work_experience_max_and_min(df, source_col="work_experience", min_col="exp_min", max_col="exp_max"):
+    def parse_exp(text):
+        if pd.isna(text):
+            return (pd.NA, pd.NA)
+        
+        t = str(text).replace(" ", "")
+        '''
+        문자열.replace(기존값, 새값, count)
+        - 기존값: 찾을 문자열
+        - 새값: 바꿀 문자열
+        - count(선택사항): 앞에서부터 몇 개만 바꿀지 선택
+        '''
+        
+        if "경력무관" in t or "무관" in t or "신입/경력" in t:
+            return (0, pd.NA)
+        if "신입" in t and "경력" not in t:
+            return(0, 0)
+        
+        # 경력에 써 있는 숫자 추출하는 부분
+        nums = re.findall(r"\d+", t)
+        nums = [int(n) for n in nums]
+
+        # 범위로 나누기 / ex). 1~3년
+        if ("~" in t or "-" in t) and len(nums) >= 2:
+            return (nums[0], nums[1])
+        
+        # 특정 기준 이상
+        if "이상" in t and len(nums) >= 1:
+            return (nums[0], pd.NA)
+        
+        # 특정 기준 이하
+        if "이하" in t and len(nums) >= 1:
+            return (pd.NA, nums[0])
+        
+        # 특정 경력 연수만 뽑는 경우
+        if len(nums) == 1:
+            return (nums[0], nums[0])
+        
+        return (pd.NA, pd.NA)
+    
+    parsed = df[source_col].apply(parse_exp)
+    '''
+    apply()는 판다스에서 함수를 각 데이터에 적용할 때 사용하는 함수임
+    Series.apply(func) -> 한 컬럼의 값마다 함수를 적용함
+    DataFrame.apply(func, axis= ...) -> 행(axis=1) 또는 열(axis=0) 단위로 함수를 적용시킴
+    ---
+    map() -> 반복 가능한 값들에 함수를 하나씩 적용하는 파이썬 기본 내장 함수
+    map(함수, 반복가능객체)
+    '''
+    df[min_col] = parsed.apply(lambda x: x[0])
+    df[max_col] = parsed.apply(lambda x: x[1])
+
+    return df
+
 def main():
     clean_df = normalize_missing_values(df, TEXT_COLUMNS)
     clean_df = remove_duplicates(clean_df, subset_col='url')
     clean_df = refined_tech_stack(clean_df, source_col="tech_stack", target_col="tech_stack")
     clean_df = location_state(clean_df, source_col="location", target_col="location_state")
     # clean_df = deadline_transform_to_datetime(clean_df, source_col='deadline', target_col='deadline_dt')
+    clean_df = education_INT_LEVEL(clean_df)
+    clean_df = work_experience_max_and_min(clean_df)
     clean_df = save_csv_with_json_lists(clean_df, list_cols=("tech_stack",))
     clean_df.to_csv(OUTPUT_PATH, index=False, encoding="utf-8-sig")
 
