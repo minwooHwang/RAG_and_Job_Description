53f410ac74f0b576a14389b635861b1fee530b6d
minwooHwang <has710312@gmail.com>
2026-02-17 17:25:01 +0900

    feat: top-k 5 -> 50으로 증가

--- rag/retriever.py
+++ rag/retriever.py
@@ -2,8 +2,8 @@
 from db.connection import get_connection
 from psycopg2.extras import RealDictCursor # psycopg2에 들어있는 딕셔너리 전용 커서 기능
 
-def retrieve_top_k(query_embedding: list[float], k: int = 5 ):
-    # 쿼리 임베딩은 리스트형식이고 안에 값은 소수점 포함, top_k는 5까지 뽑는다.
+def retrieve_top_k(query_embedding: list[float], k: int = 50 ):
+    # 쿼리 임베딩은 리스트형식이고 안에 값은 소수점 포함, top_k는 50까지 뽑는다.
     embedding_str = "[" + ",".join(map(str, query_embedding)) + "]"
 
     sql = """
